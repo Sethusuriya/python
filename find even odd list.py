083816bb1bd7ceabf51dbc1a4ee5e579275590c6
Sethusuriya <120043578+Sethusuriya@users.noreply.github.com>
2022-12-10 15:33:56 +0530

    Create find even odd list.py

---
+++ find even odd list.py
@@ -0,0 +1,10 @@
+list1=[1,5,4,6,8,1]
+oddlist=[]
+evenlist=[]
+for i in range(len(list1)):
+    if l1[i]%2==0:
+        evenlist.append(list1[i])
+    else:
+        oddlist.append(list1[i])
+print(oddlist)
+print(evenlist)
