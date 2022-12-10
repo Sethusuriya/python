45cf644951493bb5c35a76fe3da670a46de75664
Sethusuriya <120043578+Sethusuriya@users.noreply.github.com>
2022-12-10 15:40:46 +0530

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,14 @@
+salary=int(input("enter the salary amount"))
+gender=input("enter the gender (m-male/f-female")
+if gender=="m":
+    print("your bonus amount is =",5/100*salary)
+elif gender=="f":
+    print("your bonus amont is =",10/100*salary)
+
+    
+if salary<=10000:
+    print("your extra bonus amont is =",2/100*salary)
+
+    
+
+ 
